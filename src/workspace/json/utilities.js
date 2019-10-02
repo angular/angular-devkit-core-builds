9cc304d9b0c6c7b409f29382b151e4b9aead921b
Angular Builds <circleci@angular.io>
2019-10-02 18:56:55 +0000

    8497d9ac7 build: use import instead of require for fast-json-stable-stringify

--- src/workspace/json/utilities.js
+++ src/workspace/json/utilities.js
@@ -1,6 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const stableStringify = require('fast-json-stable-stringify');
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+const stableStringify = require("fast-json-stable-stringify");
 function findNode(parent, p) {
     if (parent.kind === 'object') {
         const entry = parent.properties.find(entry => entry.key.value === p);
