674f3fefc7002ad51591f002696209b1f2b50fbe
Angular Builds <circleci@angular.io>
2020-01-02 21:21:22 +0000

    d259a3644 build: update rxjs to version 6.5.4

--- src/workspace/json/utilities.js
+++ src/workspace/json/utilities.js
@@ -1,12 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
 const stableStringify = require("fast-json-stable-stringify");
 function findNode(parent, p) {
     if (parent.kind === 'object') {
