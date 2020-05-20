17a4d1de776d63ffc19b687889b764064f10573e
Angular Builds <circleci@angular.io>
2020-05-20 09:42:28 +0000

    7928de118 build: update bazel packages

--- src/exception/index.js
+++ src/exception/index.js
@@ -15,6 +15,6 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 }));
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
-};
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./exception"), exports);
