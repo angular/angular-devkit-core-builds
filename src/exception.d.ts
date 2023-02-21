b87ef678e5b8e0692541b259e4dc8b17205a8bfa
Angular Builds <circleci@angular.io>
2023-02-21 17:13:01 +0000

    0ad81cdbc refactor(@angular-devkit/core): remove deprecated exceptions

--- src/exception.d.ts
+++ src/exception.d.ts
@@ -23,33 +23,3 @@ export declare class PathIsDirectoryException extends BaseException {
 export declare class PathIsFileException extends BaseException {
     constructor(path: string);
 }
-/**
- * @deprecated since version 14. Use the same symbol from `@angular-devkit/schematics`.
- */
-export declare class ContentHasMutatedException extends BaseException {
-    constructor(path: string);
-}
-/**
- * @deprecated since version 14. Use the same symbol from `@angular-devkit/schematics`.
- */
-export declare class InvalidUpdateRecordException extends BaseException {
-    constructor();
-}
-/**
- * @deprecated since version 14. Use the same symbol from `@angular-devkit/schematics`.
- */
-export declare class MergeConflictException extends BaseException {
-    constructor(path: string);
-}
-/**
- * @deprecated since version 14. Create a custom exception implementation instead.
- */
-export declare class UnimplementedException extends BaseException {
-    constructor();
-}
-/**
- * @deprecated since version 14. Create a custom exception implementation instead.
- */
-export declare class UnsupportedPlatformException extends BaseException {
-    constructor();
-}
