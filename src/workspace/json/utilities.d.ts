db950829c9f9656c4215109a91aed99de9142052
Angular Builds <circleci@angular.dev>
2024-06-06 09:16:38 +0000

    27cb531 refactor: format bazel build files

--- src/workspace/json/utilities.d.ts
+++ src/workspace/json/utilities.d.ts
@@ -3,7 +3,7 @@
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 import { JsonArray, JsonObject, JsonValue } from '../../json';
 export type ChangeListener = (path: string[], newValue: JsonValue | undefined) => void;
