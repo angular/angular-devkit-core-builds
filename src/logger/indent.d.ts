945ccf1664619f68c330f714de81ede63ef3b434
Angular Builds <circleci@angular.dev>
2024-06-06 09:17:44 +0000

    1284b43 build: update dependency @rollup/plugin-commonjs to v26

--- src/logger/indent.d.ts
+++ src/logger/indent.d.ts
@@ -3,7 +3,7 @@
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 import { Logger } from './logger';
 export declare class IndentLogger extends Logger {
